0925235bd8679f1fd6b4baa2d50da1b4fad7b7f2
Ruoyu <klaus1201810802@gmail.com>
2022-12-06 17:10:19 +0800

    feat: 🚀 gitlab pipeline notice template

--- gitlab-pipline-notice.sh
+++ gitlab-pipline-notice.sh
@@ -4,5 +4,5 @@ WEBHOOK=${1:-$WEBHOOK_URL}
 
 curl "$WEBHOOK" \
    -H 'Content-Type: application/json' \
-   -d '{"msgtype":"template_card","template_card":{"card_type":"text_notice","main_title":{"title":"Pipeline Notification","desc":"Pipeline '$CI_COMMIT_SHORT_SHA' has failed!"},"horizontal_content_list":[{"keyname":"Author","value":"'$CI_COMMIT_AUTHOR'"},{"keyname":"Branch","value":"'$CI_COMMIT_BRANCH'"},{"keyname":"Message","value":"'$CI_COMMIT_MESSAGE'"},{"keyname":"Address","value":"Here","type":1,"url":"'$CI_PIPELINE_URL'"}],"card_action":{"type":1,"url":"'$CI_PIPELINE_URL'"}}}'
+   -d '{"msgtype":"template_card","template_card":{"card_type":"text_notice","main_title":{"title":"⚠ Pipeline Notification","desc":"Pipeline '$CI_COMMIT_SHORT_SHA' has failed!"},"horizontal_content_list":[{"keyname":"Author","value":"'$CI_COMMIT_AUTHOR'"},{"keyname":"Branch","value":"'$CI_COMMIT_BRANCH'"},{"keyname":"Message","value":"'$CI_COMMIT_MESSAGE'"},{"keyname":"Address","value":"Here","type":1,"url":"'$CI_PIPELINE_URL'"}],"card_action":{"type":1,"url":"'$CI_PIPELINE_URL'"}}}'
 
